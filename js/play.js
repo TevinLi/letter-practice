80bc69259d5b6d296a10d038c86b3544d12129c9
TevinLi <6317595@qq.com>
2017-08-20 20:21:34 +0800

    发布

---
+++ js/play.js
@@ -0,0 +1,67 @@
+/**
+ * 字母练习
+ */
+
+$(function() {
+
+    var $uppercase = $('.uppercase'),
+        $lowercase = $('.lowercase'),
+        $audioCtrl = $('#audioCtrl'),
+        $result = $('#result');
+
+    //播放音乐
+    var audio = null;
+    var audioPlay = function(type) {
+        if(!audio) {
+            audio = new Audio();
+        }
+        audio.src = 'sound/' + type + '.mp3';
+        audio.play();
+    };
+
+    //选取随机字母
+    var getLetter = function() {
+        var code = parseInt(Math.random() * 26) + 97;
+        return String.fromCharCode(code);
+    };
+
+    //播放字母
+    var playLetter = function() {
+        $uppercase.text(curLetter.toUpperCase());
+        $lowercase.text(curLetter);
+        audioPlay(curLetter);
+    }
+
+    //当前字母
+    var curLetter = getLetter();
+
+    //初始化
+    playLetter();
+
+    var keyList = {
+        c70: 'f'
+    }
+
+    //事件绑定
+    $(document).on('keydown', function(e) {
+        var keyLetter = String.fromCharCode(e.originalEvent.keyCode - 65 + 97);
+        if(keyLetter == curLetter) {
+            audioPlay('success');
+            $result.addClass('success');
+            setTimeout(function() {
+                $result.removeClass('success');
+            }, 300);
+            setTimeout(function() {
+                curLetter = getLetter();
+                playLetter();
+            }, 500);
+        } else {
+            audioPlay('error');
+            $result.addClass('error');
+            setTimeout(function() {
+                $result.removeClass('error');
+            }, 300);
+        }
+    });
+
+});
